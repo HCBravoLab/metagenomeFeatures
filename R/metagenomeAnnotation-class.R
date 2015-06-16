a2aa6c2799cfc66b3e358ec307a9efc1a09a9fac
Nate Olson <nathandavidolson@gmail.com>
2015-06-16 19:19:28 -0400

    defining object as a S4 class

--- R/metagenomeAnnotation-class.R
+++ R/metagenomeAnnotation-class.R
@@ -12,24 +12,75 @@
 
 
 ## metagenomeAnnotation class definition ----------------------------------------------
-.metagenomeAnnotation <- setRefClass("metagenomeAnnotation", contains="ShortRead",
-                              fields=list(taxa="ANY", #use of any as dplyr tbl is not a recognized class
-                                          seq="ShortRead",
-                                          features="list"
-                              ))
+# metagenomeAnnotation <- setRefClass("metagenomeAnnotation", contains="ShortRead",
+#                                      fields=list(taxa="ANY", #use of any as dplyr tbl is not a recognized class
+#                                                  seq="ShortRead",
+#                                                  features="list"
+#                                      ))
+.metagenomeAnnotation <- setRefClass("metagenomeAnnotation",
+                                    #contains=c("ShortRead", "AnnotatedDataFrame"),
+                                    representation( data="environment",
+                                                    annotation="list")
+                                    )
 
-## MetagenomeExp Validity ------------------------------------------------------
+## MetagenomeAnnotation Validity -----------------------------------------------
 ## Note the object may not necessarily have both ShortRead and taxa data.frame
 
 setValidity("metagenomeAnnotation", function(object) {
     msg <- NULL
-    if(!("seq" %in% ls(object)) || !is(object@seq, "ShortRead"))
-        msg <- paste(msg, "'seq' slot must contain a ShortRead object with sequence data", sep = "\n")
-    if(!("taxa" %in% ls(object)) || !is(object@taxa, "tbl_sqlite"))
-        msg <- paste(msg, "'taxa' slot must contain a tbl_sqlite object with taxonomy data", sep = "\n")
-    if(!("metadata" %in% ls(object)) || !is(object@metadata, "list"))
-        msg <- paste(msg, "'metadata' slot must contain a list", sep = "\n")
+    if(!(all(sapply(object@data, class) == "DataFrame")))
+        msg <- paste(msg,
+                     "All objects in 'objects@data' has to be of class 'DataFrame'",
+                     sep = "\n")
+
+#     if(!("seq" %in% ls(object)) || !is(object@seq, "ShortRead"))
+#         msg <- paste(msg, "'seq' slot must contain a ShortRead object with sequence data", sep = "\n")
+#     if(!("taxa" %in% ls(object)) || !is(object@taxa, "AnnotatedDataFrame"))
+#         msg <- paste(msg, "'taxa' slot must contain a tbl_sqlite object with taxonomy data", sep = "\n")
+#     if(!("metadata" %in% ls(object)) || !is(object@metadata, "list"))
+#         msg <- paste(msg, "'metadata' slot must contain a list", sep = "\n")
     if (is.null(msg)) TRUE else msg
 })
+
+setMethod("show", "metagenomeAnnotation", function(object) {
+    cat("metagenomeAnnotation object\n")
+    .show.annotation(object@annotation)
+    .show.availableAnnotation(object)
+})
+
+
+## MetagenomeAnnotation Class Constructor --------------------------------------
+metagenomeAnnotation <- function(listOf, annotation = ""){
+    # from minfi IlluminaMethylationAnnotation class - modify for metagenomeAnnotation
+#     stopifnot(annotation != "")
+#     stopifnot(all(c("array", "annotation", "genomeBuild") %in% names(annotation)))
+#     stopifnot(all(c("Manifest", "Locations") %in% names(listOfObjects)))
+#     Manifest <- listOfObjects[["Manifest"]]
+#     stopifnot(setequal(names(Manifest),
+#                        c("Name", "AddressA", "AddressB", "ProbeSeqA",
+#                          "ProbeSeqB", "Type", "NextBase", "Color")))
+#     stopifnot(all(sapply(listOfObjects, class) %in% c("DataFrame", "data.frame")))
+#     stopifnot(all(nrow(Manifest) == sapply(listOfObjects, nrow)))
+#     stopifnot(all(sapply(listOfObjects, function(obj) {
+#         all(rownames(obj) == rownames(Manifest))
+#     })))
+#     stopifnot(all(c("chr", "pos") %in% names(listOfObjects[["Locations"]])))
+#     stopifnot(all(listOfObjects[["Locations"]]$chr %in% .seqnames.order.all))
+#     available <- .availableAnnotation(listOfObjects)
+#     stopifnot(all(defaults %in% names(listOfObjects)))
+#     stopifnot(!anyDuplicated(sub("\\..*", "", defaults)))
+    ## FIXME: Check column names of any Islands object
+    ## Instantiating
+    data <- new.env(parent = emptyenv())
+    for(nam in names(listOfObjects)) {
+        cat(nam, "\n")
+        assign(nam, as(listOfObjects[[nam]], "DataFrame"), envir = data)
+    }
+    ## TODO - look into locking environment
+    lockEnvironment(data, bindings = TRUE)
+    anno <- new("metagenomeAnnotation",
+                annotation = annotation, data = data)
+    anno
+}
 
 ## metagenomeAnnotation methods -------------------------------------------------------
