9fa1bf64fe60df0cee5e379ab841ca9928e8be26
nate-d-olson <nolson@nist.gov>
2015-09-04 16:44:57 -0400

    bug in prototype, moved to class definition

--- R/metagenomeAnnotation-class.R
+++ R/metagenomeAnnotation-class.R
@@ -23,17 +23,12 @@ setClass("metagenomeAnnotation",
          representation = list(mgAnnotatedDF = "AnnotatedDataFrame",
                                metadata = "list",
                                featureData = "DNAStringSet"),
-         contains = c("AnnotatedDataFrame","DNAStringSet")
+         contains = c("AnnotatedDataFrame","DNAStringSet"),
+         prototype = prototype(
+             mgAnnotatedDF = new("AnnotatedDataFrame"),
+             metadata = list(),
+             featureData = new("DNAStringSet"))
 )
-
-## defining non-specified slots - empty for now
-# setMethod("prototype", "metagenomeAnnotation",
-#           function(.Object,...){
-#               mgAnnotatedDF <- new("AnnotatedDataFrame")
-#               metadata <- list()
-#               featureData <- new("DNAStringSet")
-#           }
-# )
 
 ## for use in creating a new object
 setMethod("initialize","metagenomeAnnotation",
