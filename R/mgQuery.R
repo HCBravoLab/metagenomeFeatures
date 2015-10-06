71478e455f2a22df4ad46ec97a246e134c475b9c
j.hester <j.hester@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-10-06 17:15:15 +0000

    Add DAPAR, gcatest, iCheck, Imetagene, lfa, MEAL, metagenomeFeatures, pathVar, Prostar, SICtools, SISPA, SWATH2stats
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metagenomeFeatures@109238 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/mgQuery.R
@@ -0,0 +1,13 @@
+#' Example ShortRead 16S experiment dataset
+#'
+#' A dataset containing the top OTU centers from a 16S marker gene survey.
+#' Specifically, the OTU centers came from 'Healthy and moderate to severe
+#' diarrhea 16S expression data' - bioconductor dataset 'msd16s'.
+#'
+#' @format A metagenomeFeatures object with 500 sequences.
+#'
+#' @source \url{http://bioconductor.org/packages/release/data/experiment/html/msd16s.html}
+#' @importClassesFrom ShortRead ShortRead
+#' @examples
+#' data(mgQuery)
+"mgQuery"
