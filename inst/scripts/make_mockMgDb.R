0d4d38c1106611ad554f492791416fb0e5d08e4b
nate-d-olson <nolson@nist.gov>
2016-06-29 16:59:41 -0400

    saving seq as fasta.gz, removing tree data, and cleaning up code

--- inst/scripts/make_mockMgDb.R
+++ inst/scripts/make_mockMgDb.R
@@ -1,5 +1,3 @@
-# get keys 
-
 ## Database with a random representative sequence for each genus
 
 library(dplyr)
@@ -10,42 +8,28 @@ library(ape)
 data(mock_query_df)
 
 
-# data(mock_query_df)
-
-ggtaxa <- gg13.5MgDb$taxa
-
 set.seed(1) # setting seed so that the sampled genus are the same if script is rerun
-genus_df <- ggtaxa  %>% collect() %>% group_by(Kingdom, Phylum, Class, Order, Family, Genus) %>% sample_n(1)
-write.csv(genus_df,"../extdata/gg_genus_df.csv")
+genus_df <- mgdb_taxa(gg13.5MgDb)  %>% collect() %>%
+    group_by(Kingdom, Phylum, Class, Order, Family, Genus) %>% sample_n(1)
 
+## Saving taxa data
+write.csv(genus_df, "../extdata/gg_rep_genus_df.csv")
 ## Saving keys to file
 genus_keys <- genus_df$Keys
-save(genus_keys, file = "../extdata/genus_keys.RData")
+save(genus_keys, file = "../extdata/genus_keys.rdata")
 
-# combine in query_df keys to genus_keys to
-mock_keys <- mock_query_df$Keys
-
-# mock_keys (integer), genus_keys (character)
-# combining turns everything into character
-combined_keys <- c(genus_keys, mock_keys)
+## generating dataset with both ID keys for mock dataset and single
+## representative key for each genus
+combined_keys <- c(genus_df$Keys, mock_query_df$Keys)
 
 
 # keys = combined keys
 gg_data <- mgDb_select(gg13.5MgDb, type = c("seq", "taxa"), keys = combined_keys, keytype = "Keys")
 
 
-# save as RData file
-seq_data <- gg_data$seq
+# save as fasta.gz file - saving as rdata - includes environment info, resulting in a larger than necessary file
+writeXStringSet(gg_data$seq, filepath = "../extdata/mockSeq.fasta.gz", compress = TRUE)
 
-#seq_db <- src_sqlite("../extdata/mockSeq.sqlite", create = TRUE)
-
-saveRDS(seq_data,file = "../extdata/mockSeq.rds")
-
-
-#seq_sqlite <- copy_to(seq_db, seq_data, temporary = FALSE)
-
-# turn this into sqlite and save
-taxa_data <- gg_data$taxa
-taxa_db <- src_sqlite("../extdata/mockTaxa.sqlite", create = TRUE)
-
-copy_to(taxa_db, taxa_data, name = "taxa", temporary = FALSE)
+# save taxa info as sqlite db
+src_sqlite("../extdata/mockTaxa.sqlite", create = TRUE) %>%
+    copy_to(gg_data$taxa, name = "taxa", temporary = FALSE)
