ab00bbe5665924eb7c2bb0c0fa0b7b21cf550274
nate-d-olson <nolson@nist.gov>
2016-06-29 15:49:01 -0400

    using system.file, specified data as vignette

--- R/vignette_data.R
+++ R/vignette_data.R
@@ -1,27 +1,33 @@
 ## hidden functions for vignette data
-assay_data <- function(){
-    file.path(dataDirectory, "msd16s_counts.csv") %>% metagenomeSeq::load_meta(sep = ",")
+vignette_assay_data <- function(){
+    system.file("extdata", "msd16s_counts.csv",
+                package="metagenomeFeatures") %>%
+        metagenomeSeq::load_meta(sep = ",")
 }
 
 
-pheno_data <- function(){
-    file.path(dataDirectory, "msd16s_sample_data.csv") %>%
+vignette_pheno_data <- function(){
+    system.file("extdata", "msd16s_sample_data.csv",
+                package="metagenomeFeatures")  %>%
         read.csv(row.names = 1, stringsAsFactors = F) %>%
         Biobase::AnnotatedDataFrame()
 }
 
-msd16s_MgDb <- function(){
+vignette_msd16s_MgDb <- function(){
     metadata <- list(ACCESSION_DATE = "3/31/2015",
                      URL = "https://greengenes.microbio.me",
                      DB_TYPE_NAME = "GreenGenes-MgDb-msd16s",
                      DB_TYPE_VALUE = "MgDb",
                      DB_SCHEMA_VERSION = "1.0")
 
-    db_seq <- file.path(dataDirectory,"msd16s_MgDb_seq.fasta.gz") %>%
-        readDNAStringSet()
+    db_seq <- system.file("extdata", "msd16s_MgDb_seq.fasta.gz",
+                          package="metagenomeFeatures") %>% readDNAStringSet()
 
-    new("MgDb",seq = db_seq,
-            taxa_file = file.path(dataDirectory,"msd16s_MgDb_taxa.sqlite"),
-            tree_file = file.path(dataDirectory,"msd16s_MgDb_tree.rds"),
+    new("MgDb",
+            seq = db_seq,
+            taxa_file = system.file("extdata", "msd16s_MgDb_taxa.sqlite",
+                                    package="metagenomeFeatures"),
+            tree_file = system.file("extdata","msd16s_MgDb_tree.rds",
+                                    package="metagenomeFeatures"),
             metadata = metadata)
 }
