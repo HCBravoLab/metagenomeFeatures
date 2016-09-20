cf5e0ff8fe52fedee2cd090b22a204354526f433
nate-d-olson <nathandavidolson@gmail.com>
2016-09-20 14:40:18 -0400

    using mock community data for vignette

--- inst/scripts/make_mock_query_df.R
+++ inst/scripts/make_mock_query_df.R
@@ -1,29 +1,23 @@
-#script to generate MRexperiment object
+#Generating data files for vignette and examples
 library(metagenomeSeq)
 library(biomformat)
+
+## Bug in load_biom - metagenomeSeq converting hdf5 to json biom file format
+mock_biom <- biomformat::read_hdf5_biom("../extdata/6692_analysis_dt-16S_r-1_c-3.biom")
+write_biom(mock_biom, "6692_analysis_dt_16S_r-1_c-3_biomformatv1.biom")
+
 #loading biom file as MRexperiment object
-#MRexperiment objects contain taxonomic annotations relative to associated OTUs
-mock_mrexp <- load_biom("../extdata/6692_analysis_dt-16S_r-1_c-3.biom")
+mock_mrexp <- load_biom("6692_analysis_dt_16S_r-1_c-3_biomformatv1.biom")
 
-#set otuCounts to OTU frequency per sample
+#dataframe with count table and OTU ids for generating metagenomeAnnotation
 mock_otu_counts <- MRcounts(mock_mrexp)
-
-#fData used as test to verify annotations
-
-#head(fData(mock_mrexp))
-
-#queryDF (key: rownames for each OTU, value: OTU freq)
-
-#
-# head(msd16s_query_df)
-#
-
-#set up dataframe of key ids from database with associated OTU ids
 mock_query_df <- data.frame(Keys = rownames(mock_otu_counts),OTU = 1:nrow(mock_otu_counts))
-
-#save mock_query_df as object file in appropriate folder
-#saveRDS(mock_query_df, "../data/mock_query_df.rdata")
 
 save(mock_query_df, file = "../../data/mock_query_df.RData")
 
+# saving count table data for vignettes
+exportMat(obj = mock_mrexp, log = FALSE, norm = FALSE, sep = ",",file = "../extdata/mock_counts.csv")
+
+# saving pheno data for vignettes
+write.csv(pData(mock_mrexp), file = "../extdata/mock_sample_data.csv")
 
