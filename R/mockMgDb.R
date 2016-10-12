de995f04054243887d2f64f591fc221bf00ad9b0
Nathan D. Olson <nolson@umiacs.umd.edu>
2016-10-12 21:31:35 +0000

    same code as demoMgDb
    From: nate-d-olson <nathandavidolson@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metagenomeFeatures@122310 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mockMgDb.R
+++ R/mockMgDb.R
@@ -6,34 +6,26 @@
 #' @examples
 #' get_mockMgDb()
 get_mockMgDb <- function(){
-
-
-
+    ## note same as demoMgDb
     metadata <- list(ACCESSION_DATE = "3/31/2015",
                      URL = "https://greengenes.microbio.me",
-                     DB_TYPE_NAME = "GreenGenes-MgDb-Mock",
+                     DB_TYPE_NAME = "GreenGenes",
                      DB_TYPE_VALUE = "MgDb",
-                     DB_SCHEMA_VERSION = "1.0",
-                     NOTE = "URL does not work, working link here: http://greengenes.secondgenome.com/downloads/database/13_5")
+                     DB_SCHEMA_VERSION = "1.0")
 
-    # get db seq data and filenames
-    mock_seq <- system.file("extdata", "mock_MgDb_seq.fasta.gz",
-                            package = "metagenomeFeatures") %>%
-        Biostrings::readDNAStringSet()
+    demo_seq_file <- system.file("extdata", 'mockSeq.fasta.gz',
+                                 package="metagenomeFeatures")
+    db_seq <- Biostrings::readDNAStringSet(demo_seq_file)
 
-    mock_taxa_file <- system.file("extdata", "mock_MgDb_taxa.sqlite",
-                                  package = "metagenomeFeatures")
+    demo_taxa_file <- system.file("extdata", "mockTaxa.sqlite",
+                                  package="metagenomeFeatures")
 
-    # mock_tree_file <- system.file("extdata", "msd16s_MgDb_tree.rds",
-    #                               package="metagenomeFeatures")
+    demo_tree_file <- system.file("extdata", "mockTree.rds",
+                                  package="metagenomeFeatures")
 
-
-    #  tree data slot is empty - not reference tree for gg 13.5
-    # tree_data <- mgDb_select(mgdb, type = "tree", keys = genus_keys, keytype = "Keys")
-
-    new("MgDb",
-        seq = mock_seq,
-        taxa_file = mock_taxa_file,
-        tree_file = "not available", #mock_tree_file,
-        metadata = metadata)
+    demoMgDb <- new("MgDb",
+                    seq = db_seq,
+                    taxa_file = demo_taxa_file,
+                    tree_file = demo_tree_file,
+                    metadata = metadata)
 }
