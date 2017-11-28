1c1473aba370dd7de201a4d9da20336661bd4530
Nate Olson <nolson@nist.gov>
2017-11-28 15:54:51 -0500

    updating test MgDb to new class definition

--- tests/testthat/helper-MgDb.R
+++ tests/testthat/helper-MgDb.R
@@ -6,9 +6,9 @@ test_metadata <- list(ACCESSION_DATE = "1/11/1111",
                       URL = "test-data",
                       DB_TYPE_NAME = "Test",
                       DB_TYPE_VALUE = "MgDb",
-                      DB_SCHEMA_VERSION = "1.0")
+                      DB_SCHEMA_VERSION = "2.0")
 
-test_taxa_file <- "../test_taxa.sqlite3"
+test_db_file <- "../test_db.sqlite3"
 
 test_seq_file <- "../test_seq.rds"
 test_seq <- readRDS(test_seq_file)
@@ -16,18 +16,17 @@ test_seq <- readRDS(test_seq_file)
 test_tree_file <- "../test_tree.rds"
 test_tree <- readRDS(test_tree_file)
 
-testMgDb <- new("MgDb", seq = test_seq,
-                taxa_file = test_taxa_file,
-                tree_file = test_tree_file,
-                metadata = test_metadata)
+## Update to work with new class definition
+testMgDb <- MgDb(db_file = test_db_file, tree = test_tree_file, metadata = test_metadata)
 
 
 ## data.frame for testing select methods
 tax_names <- matrix(paste0("tax_",0:69), ncol = 7)
-colnames(tax_names) <- c("Kingdom","Phylum","Class","Order",
+colnames(tax_names) <- c("Kingdom","Phylum","Class","Ord",
                          "Family","Genus","Species")
 taxa <- data.frame(Keys = as.character(1:10), tax_names,
-                   stringsAsFactors = FALSE) %>% dplyr::as_data_frame()
+                   stringsAsFactors = FALSE)
+taxa <- dplyr::as_data_frame(taxa)
 
 
 OTU <- c(4,4,4,3,2)
