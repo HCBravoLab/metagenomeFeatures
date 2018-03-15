4c07c4eaba20530dcdee111b3f8f85f4b99d3ca7
Nate Olson <nolson@nist.gov>
2018-03-15 11:01:07 -0400

    modified fun names, docs, and return values

--- tests/testthat/test_agg_tax.R
+++ tests/testthat/test_agg_tax.R
@@ -32,97 +32,22 @@ context("aggregate_taxa")
 
 ## agg_taxa fails if only one sample is present in count, unable to assign row names for n X 1 matrix
 
-test_that("aggregate_taxa-return-class",{
-    expect_is(aggregate_taxonomy(testMgDb, taxa_level = "Species")[["agg_taxa_table"]],"tbl")
-    expect_is(aggregate_taxonomy(testMgDb, taxa_level = "Species", mapping = TRUE)[["agg_taxa_mapping"]],"tbl")
+test_that("mgDb_aggregate_taxa-return-class",{
+    expect_is(mgDb_aggregate_taxonomy(testMgDb, taxa_level = "Species"),"tbl")
+    expect_is(mgDb_aggregate_taxonomy(testMgDb, taxa_level = "Species", mapping = TRUE)[["agg_taxa_mapping"]],"tbl")
 })
 
 ## tests for wrong input
 
 ## test for wrong params
-test_that("aggregate_taxa-params", {
-    expect_error(aggregate_taxa("not MRexp", taxa_level = "Species"))
-    expect_error(aggregate_taxa(testMgDb, taxa_level = 10))
-    expect_error(aggregate_taxa(testMgDb, taxa_level = "NOT a level"))
-    expect_error(aggregate_taxa(testMgDb, taxa_level = "Species", mapping = "Wrong!"))
-    ##  no current check, how to check function is appropriate for matrix?
-    # expect_error(aggregate_taxa(test_MRexp, lvl = "Species", aggFun = "BAD"))
-    ## silent arguments - move checks to MRcounts
-    # expect_warning(aggregate_taxa(test_MRexp, lvl = "Species", sl = "BAD"))
-    # expect_error(aggregate_taxa(test_MRexp, lvl = "Species", norm = "BAD"))
-    # expect_error(aggregate_taxa(test_MRexp, lvl = "Species", log = "BAD"))
+test_that("mgDb_aggregate_taxa-params", {
+    expect_error(mgDb_aggregate_taxa("not MgDb", taxa_level = "Species"))
+    expect_error(mgDb_aggregate_taxa(testMgDb, taxa_level = 10))
+    expect_error(mgDb_aggregate_taxa(testMgDb, taxa_level = "NOT a level"))
+    expect_error(mgDb_aggregate_taxa(testMgDb, taxa_level = "Species", mapping = "Wrong!"))
 })
 
-test_that("aggregate_taxa_mgf-return-class",{
-    expect_is(aggregate_taxonomy_mgf(test_mgF, taxa_level = "Species")[["agg_taxa_table"]],"DataFrame")
-    expect_is(aggregate_taxonomy_mgf(test_mgF, taxa_level = "Species", mapping = TRUE)[["agg_taxa_mapping"]],"DataFrame")
+test_that("mgF_aggregate_taxa-return-class",{
+    expect_is(mgF_aggregate_taxonomy(test_mgF, taxa_level = "Species"),"DataFrame")
+    expect_is(mgF_aggregate_taxonomy(test_mgF, taxa_level = "Species", mapping = TRUE)[["agg_taxa_mapping"]],"DataFrame")
 })
-
-## tests for wrong input
-
-# ## test for wrong params
-# test_that("aggregate_taxa-params", {
-#     expect_error(aggregate_taxa("not MRexp", taxa_level = "Species"))
-#     expect_error(aggregate_taxa(testMgDb, taxa_level = 10))
-#     expect_error(aggregate_taxa(testMgDb, taxa_level = "NOT a level"))
-#     expect_error(aggregate_taxa(testMgDb, taxa_level = "Species", mapping = "Wrong!"))
-#     ##  no current check, how to check function is appropriate for matrix?
-#     # expect_error(aggregate_taxa(test_MRexp, lvl = "Species", aggFun = "BAD"))
-#     ## silent arguments - move checks to MRcounts
-#     # expect_warning(aggregate_taxa(test_MRexp, lvl = "Species", sl = "BAD"))
-#     # expect_error(aggregate_taxa(test_MRexp, lvl = "Species", norm = "BAD"))
-#     # expect_error(aggregate_taxa(test_MRexp, lvl = "Species", log = "BAD"))
-# })
-#
-#
-# ## test for single sample
-# test_that("aggregate_taxa-single sample", {
-#     expect_error(aggregate_taxa(test_MRexp1, lvl = "Species"))
-# })
-#
-# ## test for undefined taxa (NA)
-#
-#
-#
-# ## tests for count values
-# test_that("aggregate_taxa-matrix value", {
-#     exp_colnames <- paste0("sam",1:2)
-#
-#     ## check Species level agg
-#     exp_rownames <- paste0("tax_",c(60,62:69))
-#     exp_mat <- matrix(data = c(3,3:10,5,(3:10)^2), ncol = 2,
-#                       dimnames = list(exp_rownames, exp_colnames))
-#     expect_equal(aggregate_taxa(test_MRexp, lvl = "Species", out = "matrix"),
-#                  exp_mat)
-#
-#     ## check Genus level agg
-#     exp_rownames <- paste0("tax_",c(50,52,54:59))
-#     exp_mat <- matrix(data = c(3,7,5:10,5,25,(5:10)^2), ncol = 2,
-#                       dimnames = list(exp_rownames, exp_colnames))
-#     expect_equal(aggregate_taxa(test_MRexp, lvl = "Genus", out = "matrix"),
-#                  exp_mat)
-#
-#     ## check Family level agg
-#     exp_rownames <- paste0("tax_",c(40,42,44,46:49))
-#     exp_mat <- matrix(data = c(3,7,11,7:10,5,25,61,(7:10)^2), ncol = 2,
-#                       dimnames = list(exp_rownames, exp_colnames))
-#     expect_equal(aggregate_taxa(test_MRexp, lvl = "Family", out = "matrix"),
-#                  exp_mat)
-#
-#     ## add tests for other maxtrix sum opperations
-# })
-#
-# ## test for all unique
-# test_that("aggregate_taxa-single sample", {
-#     expect_message(aggregate_taxa(test_MRexp_all_unique, lvl = "Species"))
-#     ## need to think about best test dataset for when the user specified level has only unique values
-#     ## expect_message(aggregate_taxa(test_MRexp_lvl_unique, lvl = "Species"))
-# })
-#
-# ## test for taxa_levels
-# test_that("taxa_levels", {
-#     expect_is(taxa_levels(test_MRexp), "character")
-#     expect_error(taxa_levels("not mr_exp"))
-#     exp_val <- c("OTU","Kingdom","Phylum","Class","Order","Family","Genus","Species")
-#     expect_equal(taxa_levels(test_MRexp), exp_val)
-# })
