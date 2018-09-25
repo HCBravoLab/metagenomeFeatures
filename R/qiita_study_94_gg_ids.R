81bd2504797074ad2170b13b53b1ef196c0a14b2
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2018-09-25 14:22:48 -0400

    convert qiita data to rda & move to data folder

---
+++ R/qiita_study_94_gg_ids.R
@@ -0,0 +1,7 @@
+#' QIITA Dataset
+#' For this vignette we are using 16S rRNA data from Rousk et al. 2010, a soil microbiome study, https://qiita.ucsd.edu/study/description/94.
+#' A BIOM and qiime mapping file for the study can be obtained from QIITA.
+#' A vector of Greengenes for the study cluster centers is included in this package for use in this vignette.
+#'
+#' data(qiita_study_94_gg_ids)
+"qiita_study_94_gg_ids"
